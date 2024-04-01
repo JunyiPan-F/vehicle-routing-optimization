f2c48f076ffa721e18d032874c39e8192fb632d1
yix225 <yix225@lehigh.edu>
2024-04-01 12:50:18 -0400

    the basic idea of tsp and test

---
+++ test.cpp
@@ -0,0 +1,35 @@
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include "vrp.h"
+
+using namespace std;
+
+int main() {
+    vector<Customer> customers = {
+        {0, 0, 0, 0, 0, 0, 0},
+        {1, 1, 1, 1, 1, 1, 1},
+        {2, 2, 2, 2, 2, 2, 2},
+        {3, 3, 3, 3, 3, 3, 3},
+        {4, 4, 4, 4, 4, 4, 4},
+        {5, 5, 5, 5, 5, 5, 5},
+        {6, 6, 6, 6, 6, 6, 6},
+        {7, 7, 7, 7, 7, 7, 7},
+        {8, 8, 8, 8, 8, 8, 8},
+        {9, 9, 9, 9, 9, 9, 9}
+    };
+    vector<Vehicle> vehicles = {
+        {10, 0, 0, 0, {}},
+        {10, 0, 0, 0, {}},
+        {10, 0, 0, 0, {}}
+    };
+
+    divideConquer(customers, vehicles);
+
+    vector<int> subset = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    vector<vector<double>> memo(10, vector<double>(9, -1));
+    cout << tsp(customers, subset, 0, memo) << endl;
+    return 0;
+}
---
+++ vrp.h
@@ -0,0 +1,91 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+
+using namespace std;
+
+const int num_customer;
+const int num_vehicle;
+
+struct Customer {
+    int id;
+    int x;
+    int y;
+    int demand;
+    int ready_time;
+    int due_time;
+    int service_time;
+};
+
+struct Vehicle {
+    int capacity;
+    int current_load;
+    int current_time;
+    int current_location;
+    vector<int> route;
+};  
+
+double distance(const Customer& a, const Customer& b) {
+    return sqrt(pow(a.x - b.x, 2) + pow(a.y - b.y, 2));
+}
+
+// using the TSP  and nearest neighbor algorithm to solve the VRP、
+
+double tsp(const vector<Customer>& customers, const vector<int>& subset, int start, vector<vector<double>>& memo) {
+    if (subset.empty())
+        return distance(customers[start], customers[0]); // Return distance to the starting point
+
+    int n = subset.size();
+    if (memo[start][n] != -1)
+        return memo[start][n];
+
+    double minDist = numeric_limits<double>::max();
+    for (int i = 0; i < n; ++i) {
+        int next = subset[i];
+        vector<int> remaining;
+        for (int j = 0; j < n; ++j) {
+            if (j != i)
+                remaining.push_back(subset[j]);
+        }
+        double dist = distance(customers[start], customers[next]) + tsp(customers, remaining, next, memo);
+        minDist = min(minDist, dist);
+    }
+
+    memo[start][n] = minDist;
+    return minDist;
+}
+
+// int nearestNeighbor(const vector<Customer>& customers, vector<int>& subset, int start) {
+//     int n = subset.size();
+//     int nearest = -1;
+//     double minDist = numeric_limits<double>::max();
+//     for (int i = 0; i < n; ++i) {
+//         int next = subset[i];
+//         double dist = distance(customers[start], customers[next]);
+//         if (dist < minDist) {
+//             minDist = dist;
+//             nearest = next;
+//         }
+//     }
+//     return nearest;
+// }
+
+void divideConquer(const vector<Customer>& customers, vector<int>& vehicles) {
+    vector<vector<int>> clusters(num_vehicle);
+
+    for (int i = 1; i < num_customer; ++i) {
+        clusters[i % num_vehicle].push_back(i);
+    }
+
+    // Solve TSP for each cluster
+    for (int i = 0; i < num_vehicle; ++i) {
+        vector<int>& cluster = clusters[i];
+        vector<vector<double>> memo(num_customer, vector<double>(cluster.size() + 1, -1));
+        double minDist = tsp(customers, cluster, 0, memo);
+        cout << "Cluster " << i+1 << " distance: " << minDist << endl;
+    }
+}
+
+
+
